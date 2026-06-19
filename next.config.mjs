c99b73c1505d158d1968fc89794b4c2457440e97
haraken01-01 <haraken01@gmail.com>
2026-06-19 13:17:11 +0900

    Fix GitHub Pages route output

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ const isGithubPages = process.env.GITHUB_ACTIONS === "true";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  trailingSlash: true,
   basePath: isGithubPages ? "/dog-food-app" : "",
   assetPrefix: isGithubPages ? "/dog-food-app/" : "",
   images: {
